3850bfe58ab90b41e8c5fe8478ce7c30fa319420
raspy-on-osu <email.jack.avery@gmail.com>
2022-08-11 14:02:38 -0400

    move samples to main, add sample, fix imports

--- main.rs
+++ main.rs
@@ -1,9 +1,38 @@
 mod serv;
-use serv::api::{echo, HTTPEndpointHandler};
+use serv::{api, http};
+use std::num::ParseIntError;
 
 fn main() {
-    let mut endpoints = HTTPEndpointHandler::new();
+    let mut endpoints = api::HTTPEndpointHandler::new();
     endpoints.add("/echo", echo);
+    endpoints.add("/mult", example_multbytwo);
 
     endpoints.serve(8080);
+}
+
+pub fn echo(req: &http::Request) -> http::Response {
+    let mut res = http::Response::new(http::Code::Ok);
+
+    res.mod_header("Content-Type", "text/html; charset=utf-8");
+
+    res.body = req.clone().path;
+
+    res
+}
+
+fn example_multbytwo(req: &http::Request) -> http::Response {
+    let mut res = http::Response::new(http::Code::Ok);
+
+    if let Some(n) = req.get_param("number") {
+        let num: Result<i32, ParseIntError> = n.trim().parse();
+
+        match num {
+            Ok(i) => res.body = (i * 2).to_string(),
+            Err(e) => {
+                res = api::gen500(e.to_string());
+            }
+        }
+    }
+
+    res
 }
